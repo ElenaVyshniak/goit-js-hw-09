2adcb473ddba6cc8b49b5ff67ea56c9fd4b90d87
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-21 14:47:11 +0000

    Deploying to gh-pages from @ ElenaVyshniak/goit-js-hw-09@f72922332f978968d73e8e24c1be4db68c2b0784 🚀

---
+++ 01-color-switcher.5ce0dc80.js
@@ -0,0 +1,2 @@
+document.querySelector("button").addEventListener("click",(function(){var e=setTimeout((function(){console.log("Start")}),2e3);console.log(e)}));
+//# sourceMappingURL=01-color-switcher.5ce0dc80.js.map
---
+++ 01-color-switcher.a15b42b4.js
@@ -0,0 +1,2 @@
+document.querySelector("button").addEventListener("click",(()=>{const e=setTimeout((()=>{console.log("Start")}),2e3);console.log(e)}));
+//# sourceMappingURL=01-color-switcher.a15b42b4.js.map
