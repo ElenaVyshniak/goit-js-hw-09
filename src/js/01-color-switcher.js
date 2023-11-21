f72922332f978968d73e8e24c1be4db68c2b0784
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-21 16:46:45 +0200

    спроба

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
+const button = document.querySelector('button');
 
+const onClick = () => {
+  const timerId = setTimeout(() => {
+    console.log('Start');
+  }, 2000);
+
+  console.log(timerId);
+};
+
+button.addEventListener('click', onClick);
