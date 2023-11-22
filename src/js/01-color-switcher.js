70d0a215547dfa30839a052bd293008c593e4c2a
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-22 11:39:12 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,26 @@
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-const onClick = () => {
-  const timerId = setTimeout(() => {
-    console.log('Start');
-  }, 2000);
+// const onClick = () => {
+//   const timerId = setTimeout(() => {
+//     console.log('Start');
+//   }, 2000);
 
-  console.log(timerId);
-};
+//   console.log(timerId);
+// };
 
-button.addEventListener('click', onClick);
+// button.addEventListener('click', onClick);
+
+const startBtn = document.querySelector('.js-start');
+const stopBtn = document.querySelector('.js-stop');
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    console.log(`I love async JS!  ${Math.random()}`);
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
